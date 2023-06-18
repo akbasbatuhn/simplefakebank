742a9d348fb8de433df6d059d7713f6f51c04679
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-06-18 21:34:57 +0300

    Add dtos

--- src/main/java/com/fakedevbankapp/account/dto/converter/CustomerDtoConverter.java
+++ src/main/java/com/fakedevbankapp/account/dto/converter/CustomerDtoConverter.java
@@ -1,11 +1,20 @@
 package com.fakedevbankapp.account.dto.converter;
 
 import com.fakedevbankapp.account.dto.AccountCustomerDto;
+import com.fakedevbankapp.account.dto.CustomerDto;
 import com.fakedevbankapp.account.model.Customer;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
 public class CustomerDtoConverter {
+
+    private final CustomerAccountDtoConverter converter;
+
+    public CustomerDtoConverter(CustomerAccountDtoConverter converter) {
+        this.converter = converter;
+    }
 
     public AccountCustomerDto convertToAccountCustomer(Customer from) {
         if(from == null) return new AccountCustomerDto("", "", "");
@@ -14,6 +23,15 @@ public class CustomerDtoConverter {
                 from.getId(),
                 from.getName(),
                 from.getSurname()
+        );
+    }
+
+    public CustomerDto convertToCustomerDto(Customer from) {
+        return new CustomerDto(
+                from.getId(),
+                from.getName(),
+                from.getSurname(),
+                from.getAccount().stream().map(converter::convert).collect(Collectors.toSet())
         );
     }
 }
