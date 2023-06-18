b80aa7257914beca9888ed44cd4fd5e85a72acc9
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-06-18 21:36:53 +0300

    Add CommandLineRunner for test

--- src/main/java/com/fakedevbankapp/account/AccountApplication.java
+++ src/main/java/com/fakedevbankapp/account/AccountApplication.java
@@ -1,13 +1,30 @@
 package com.fakedevbankapp.account;
 
+import com.fakedevbankapp.account.model.Customer;
+import com.fakedevbankapp.account.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountApplication {
+public class AccountApplication implements CommandLineRunner {
+
+	private final CustomerRepository customerRepository;
+
+	public AccountApplication(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Customer customer = customerRepository.save(new Customer("Batuhan", "Akbas"));
+		Customer customer2 = customerRepository.save(new Customer("John", "Doe"));
+
+		System.out.println(customer);
+		System.out.println(customer2);
+	}
 }
